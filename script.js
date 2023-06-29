13fafbc2ef24ecc457ccce7291b68fa37620237b
soumyadeep-itobuz <soumyadeep@itobuz.com>
2023-06-29 18:36:15 +0530

    updated

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const arr = [];
 const person = document.querySelector("#name");
 const amount = document.querySelector("#money");
-const moneyUpdated = document.querySelector("#moneyUpdated");
-let flag = 0;
+const moneyUpdated = document.querySelector("#money-updated");
 
 document.querySelector(".updated").classList.add("hidden");
 document.querySelector(".display").classList.add("hidden");
+
 function submit() {
   if (person.value.trim() === "" || amount.value.trim() === "") {
     alert("Empty Field Not Allowed");
@@ -24,10 +24,10 @@ function submit() {
     <button onclick="update(${i})">Update</button></div>`;
     }
   }
-
   person.value = "";
   amount.value = "";
 }
+
 function update(i) {
   document.querySelector(".updatedButton").innerHTML = "";
   document.querySelector(".inputContainer").classList.add("hidden");
@@ -39,12 +39,14 @@ function update(i) {
   <button class="bg-danger mt-2 p-1 text-white"onclick="check(${i})">Money Update</button>`;
   moneyUpdated.value = "";
 }
+
 function check(i) {
   console.log(moneyUpdated.value);
   console.log(arr[i].money);
   arr[i].money = Number(arr[i].money) + Number(moneyUpdated.value);
   show(i);
 }
+
 function show(i) {
   document.querySelector(".display").classList.remove("hidden");
   document.querySelector(".updated").classList.add("hidden");
