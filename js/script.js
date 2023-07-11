fe1c19a84fecf56b5c0f0a13b4550f578df19fea
soumyadeep-itobuz <soumyadeep@itobuz.com>
2023-07-11 15:25:18 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const arr = [];
+const arr = JSON.parse(localStorage.getItem("arr")) || [];
 const person = document.querySelector("#name");
 const amount = document.querySelector("#money");
 const moneyUpdated = document.querySelector("#money-updated");
@@ -10,10 +10,11 @@ updatedDisplay.classList.add("hidden");
 display.classList.add("hidden");
 
 function submit() {
-  if (!person.value.trim() || !amount.value.trim() || amount.value <= 0) {
+  if (!person.value.trim() || !amount.value.trim() || amount.value <= 0) {    
     alert("Empty Field Not Allowed");
   } else {
     arr.push({ name: person.value, money: amount.value });
+    localStorage.setItem("arr", JSON.stringify(arr));
     show();
   }
   person.value = "";
@@ -24,19 +25,26 @@ function update(i) {
   updatedButton.innerHTML = "";
   display.classList.remove("hidden");
   updatedDisplay.classList.remove("hidden");
-  updatedButton.innerHTML += `
-  <button class="bg-success align-self-end p-1 text-white"onclick="add(${i})">Add</button>
-  <button class="bg-danger align-self-end  p-1 text-white"onclick="sub(${i})">Sub</button>`;
+  updatedButton.innerHTML += `<button class="bg-success align-self-end p-1 text-white"onclick="addMoney(${i})">Add</button>
+  <button class="bg-danger align-self-end  p-1 text-white"onclick="subMoney(${i})">Sub</button>`;
   moneyUpdated.value = "";
 }
 
-function add(i) {
-  arr[i].money = Number(arr[i].money) + Number(moneyUpdated.value);
+function removeItem(i) {
+  arr.splice(i, 1);
+  localStorage.setItem("arr", JSON.stringify(arr));
   show();
 }
 
-function sub(i) {
+function addMoney(i) {
+  arr[i].money = Number(arr[i].money) + Number(moneyUpdated.value);
+  localStorage.setItem("arr", JSON.stringify(arr));
+  show();
+}
+
+function subMoney(i) {
   arr[i].money = Number(arr[i].money) - Number(moneyUpdated.value);
+  localStorage.setItem("arr", JSON.stringify(arr));
   show();
 }
 
@@ -47,6 +55,16 @@ function show() {
   for (let i = 0; i < arr.length; i++) {
     display.innerHTML += `<div class="d-flex justify-content-center gap-5"><span class="align-self-center">Name : ${arr[i].name}</span>
     <span class="align-self-center">Money Borrowed : ${arr[i].money}</span>
-    <button class="bg-warning" onclick="update(${i})">Update</button></div>`;
+    <button class="bg-success" onclick="update(${i})">Update</button>
+    <button class="bg-danger text-white" onclick="removeItem(${i})">Delete</button>
+    </div>`;
   }
 }
+
+function removeAll() {
+  arr.splice(0, arr.length);
+  show();
+  localStorage.setItem("arr", JSON.stringify(arr));
+}
+
+show();
