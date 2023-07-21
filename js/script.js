4226c023f13b199434aeb715469c5234b4665c22
soumyadeep-itobuz <soumyadeep@itobuz.com>
2023-07-21 16:10:35 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -7,16 +7,20 @@ const updatedDisplay = document.querySelector(".updated");
 const updatedButton = document.querySelector(".updatedButton");
 const submitButton = document.querySelector(".submit-button");
 
-money.addEventListener("keydown", function (e) {
-  if (e.code === "Enter") submit();
-});
-person.addEventListener("keydown", function (e) {
-  if (e.code === "Enter") submit();
-});
+let flag = 0;
+let temp = 0;
 
 function submit() {
+  for (let i = 0; i < arrayList.length; i++) {
+    if (arrayList[i].name.toLowerCase() === person.value.trim().toLowerCase()) {
+      temp = 1;
+    }
+  }
   if (!person.value.trim() || !amount.value.trim() || amount.value <= 0) {
     alert("Empty Field Not Allowed");
+  } else if (temp === 1) {
+    alert("Name already exist");
+    temp = 0;
   } else {
     arrayList.push({
       name: person.value,
@@ -31,13 +35,15 @@ function submit() {
 }
 
 function show() {
+  person.removeAttribute("readonly", true);
+  flag = 0;
   submitButton.classList.remove("hidden");
   updatedButton.classList.add("hidden");
   person.value = "";
   amount.value = "";
   display.innerHTML = "";
   for (let i = 0; i < arrayList.length; i++) {
-    display.innerHTML += `<div class="list d-flex justify-content-center gap-5"><p class="align-self-center w-100 ">Name : ${arrayList[i].name}</p>
+    display.innerHTML += `<div class="list d-flex flex-column flex-md-row justify-content-center gap-5"><p class="align-self-center w-100 ">Name : ${arrayList[i].name}</p>
     <p class="align-self-center w-100">Money Borrowed : ${arrayList[i].money}</p>
     <button class="bg-success py-1 px-4 border-0 rounded-1" onclick="update(${arrayList[i].id})"><img src="../svg/pencil-solid.svg" alt="trash"></button>
     <button class="bg-danger text-white  py-1 px-4 border-0 rounded-1" onclick="removeItem(${arrayList[i].id})"><img src="../svg/trash-solid.svg" alt="trash"></button>
@@ -46,9 +52,11 @@ function show() {
 }
 
 function update(id) {
+  flag = 1;
   arrayList.map((element) => {
     if (id === element.id) {
       person.value = element.name;
+      person.setAttribute("readonly", true);
     }
   });
   submitButton.classList.add("hidden");
@@ -66,6 +74,7 @@ function removeItem(id) {
 }
 
 function addMoney(id) {
+  if (amount.value <= 0 || !amount.value) alert("Enter a valid amount");
   arrayList.map((element) => {
     if (id === element.id) {
       element.money = Number(element.money) + Number(amount.value);
@@ -78,6 +87,7 @@ function addMoney(id) {
 }
 
 function subMoney(id) {
+  if (amount.value <= 0 || !amount.value) alert("Enter a valid amount");
   arrayList.map((element) => {
     if (id === element.id) {
       element.money = Number(element.money) - Number(amount.value);
@@ -94,5 +104,12 @@ function removeAll() {
   show();
   localStorage.clear();
 }
+
+money.addEventListener("keydown", (e) => {
+  if (e.code === "Enter" && flag === 0) submit();
+});
+person.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") submit();
+});
 
 show();
